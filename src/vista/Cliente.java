ca0a4af709456bf79b6332b2cf5eb5b7690100ae
tephyMoon <tephy.andrinov@gmail.com>
2024-07-30 22:43:50 -0600

    Primer commit

---
+++ src/vista/Cliente.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Maritza Vargas
+ */
+public class Cliente extends Persona{
+    private String nit;
+
+    public Cliente(){}
+    public Cliente(String nit, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.nit = nit;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    @Override
+    protected String[] crear (){
+        
+            try{         
+                    String datos[]= new String[6];      
+                      datos[0] = getNit();
+                      datos[1] = getNombres();
+                      datos[2] = getApellidos();
+                      datos[3] = getDireccion();
+                      datos[4] = getTelefono();
+                      datos[5] = getFecha_nacimiento();
+              return  datos;            
+                 }
+                 
+        catch(Exception ex){
+                  System.out.println("algo salio mal: "+ ex.getMessage());
+                  return null;
+            }
+    }
+    
+    
+}
